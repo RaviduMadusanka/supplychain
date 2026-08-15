7233081cabb2b7b9dd7136e057df53a7cabae27f
Ravidu Madushanka <ravidumadusanka338@gmail.com>
2026-08-15 13:45:06 +0530

    update

--- core/src/main/java/com/globaltrade/core/entity/InventoryItem.java
+++ core/src/main/java/com/globaltrade/core/entity/InventoryItem.java
@@ -27,7 +27,7 @@ public class InventoryItem implements Serializable {
     @Column(name = "reorder_level")
     private Integer reorderLevel = 10;
 
-    // Getters and Setters
+
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
     public String getSku() { return sku; }
