464c640ad4f79da850f48b0096a67fa180bf8bc1
Ravidu Madushanka <ravidumadusanka338@gmail.com>
2026-08-18 13:13:57 +0530

    update

--- core/src/main/java/com/globaltrade/core/service/AuthService.java
+++ core/src/main/java/com/globaltrade/core/service/AuthService.java
@@ -1,7 +1,9 @@
 package com.globaltrade.core.service;
 
 import com.globaltrade.core.dto.UserDTO;
+import jakarta.ejb.Local;
 
+@Local
 public interface AuthService {
     UserDTO authenticate(String username, String plainPassword) throws Exception;
 }
